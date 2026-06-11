afe05aa755a0b98c462549a737a6b6b672c06842
bunnys121 <bs.ebenezer@hcltech.com>
2026-06-11 10:07:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "POC-7: GitHub + Jenkins + Docker + Ansible Deployment Successful!"
+    return "POC-7: GitHub + Jenkins + Docker + Ansible!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
